c7469ecd7d7c4fe5eb15f56bf04895e03b5aa97f
Rob N <walrusk@gmail.com>
2025-11-12 13:13:00 -0600

    Dalmation machine clarity

--- tests/Fixtures/DalmationMachine.php
+++ tests/Fixtures/DalmationMachine.php
@@ -12,27 +12,25 @@ class DalmationMachine
     public static function make(): Machine
     {
         return new Machine(
-            [DalmationState::S0, DalmationState::S1, DalmationState::S2, DalmationState::S3],
+            [DalmationState::S0, DalmationState::S1, DalmationState::S10, DalmationState::S101],
             ["0", "1"],
             DalmationState::S0,
             [false, false, false, true],
             function (int $state, string $input) {
-                if ($state === DalmationState::S3) { // Found 101 already.
-                    return $state;
+                $bit = $input === '1' ? 1 : 0;
+
+                switch ($state) {
+                    case DalmationState::S0:
+                        return $bit ? DalmationState::S1 : DalmationState::S0;
+                    case DalmationState::S1:
+                        return $bit ? DalmationState::S1 : DalmationState::S10;
+                    case DalmationState::S10:
+                        return $bit ? DalmationState::S101 : DalmationState::S0;
+                    case DalmationState::S101:
+                        return DalmationState::S101;
                 }
 
-                $numberSoFar = [
-                    '',   // S0
-                    '1',  // S1
-                    '10', // S2
-                ];
-
-                return match ($numberSoFar[$state] . $input) {
-                    '1', '11' => DalmationState::S1,
-                    '10' => DalmationState::S2,
-                    '101' => DalmationState::S3,
-                    default => DalmationState::S0,
-                };
+                throw new \Exception('Invalid state: ' . $state);
             },
         );
     }
@@ -42,6 +40,6 @@ final class DalmationState
 {
     public const int S0 = 0; // Nothing found.
     public const int S1 = 1; // Last symbol was 1.
-    public const int S2 = 2; // Last two symbols were 10.
-    public const int S3 = 3; // 101 found!
+    public const int S10 = 10; // Last two symbols were 10.
+    public const int S101 = 101; // 101 found!
 }
