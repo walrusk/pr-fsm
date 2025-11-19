b78b257d3fb06cf65fb69ab84b82d7169d2bd483
Rob N <walrusk@gmail.com>
2025-11-19 12:34:26 -0600

    update dalmation example

--- examples/dalmation.php
+++ examples/dalmation.php
@@ -8,23 +8,25 @@ use PrFsm\Machine;
  * Machine determines if 101 appears in a binary number.
  */
 $mod3machine = new Machine(
-    [DalmationState::S0, DalmationState::S1, DalmationState::S2, DalmationState::S3],
+    [DalmationState::S0, DalmationState::S1, DalmationState::S10, DalmationState::S101],
     ["0", "1"],
     DalmationState::S0,
     [false, false, false, true],
     function (int $state, string $input) {
-        $numberSoFar = [
-            '',    // S0
-            '1',   // S1
-            '10',  // S2
-            '101', // S3
-        ];
-        return match ($numberSoFar[$state] . $input) {
-            '1', '11' => DalmationState::S1,
-            '10' => DalmationState::S2,
-            '101', '1010', '1011' => DalmationState::S3,
-            default => DalmationState::S0,
-        };
+        $bit = $input === '1' ? 1 : 0;
+
+        switch ($state) {
+            case DalmationState::S0:
+                return $bit ? DalmationState::S1 : DalmationState::S0;
+            case DalmationState::S1:
+                return $bit ? DalmationState::S1 : DalmationState::S10;
+            case DalmationState::S10:
+                return $bit ? DalmationState::S101 : DalmationState::S0;
+            case DalmationState::S101:
+                return DalmationState::S101;
+        }
+
+        throw new \Exception('Invalid state: ' . $state);
     },
 );
 
@@ -32,8 +34,8 @@ final class DalmationState
 {
     public const int S0 = 0; // Nothing found.
     public const int S1 = 1; // Last symbol was 1.
-    public const int S2 = 2; // Last two symbols were 10.
-    public const int S3 = 3; // 101 found!
+    public const int S10 = 10; // Last two symbols were 10.
+    public const int S101 = 101; // 101 found!
 }
 
 $output = $mod3machine->process('1101');
